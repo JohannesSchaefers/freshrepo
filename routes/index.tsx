23fefae7dc76caea3083a6f44a671dbb0ab69f04
JohannesSchaefers <info@iconicity.de>
2023-11-27 00:03:53 +0100

    48

--- routes/index.tsx
+++ routes/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           height="128"
           alt="the Fresh logo: a sliced lemon dripping with juice"
         />
-        <h1 class="text-4xl font-bold">Welcome to the 47th biggest Fresh in town!</h1>
+        <h1 class="text-4xl font-bold">Welcome to the 48th biggest Fresh in town!</h1>
         <p class="my-4">
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
