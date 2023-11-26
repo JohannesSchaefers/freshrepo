d63e193550e391463528bf942458a5a51d895dd1
johannesschaefers <info@iconicity.de>
2023-11-26 21:47:40 +0100

    10th biggest commit

--- routes/index.tsx
+++ routes/index.tsx
@@ -29,34 +29,4 @@ export default function Home() {
     }
 
 
-  /*
- return (
-
-  <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Simple Bar Chart</title>
-  <style>
-    canvas {
-      border: 1px solid #ccc;
-    }
-  </style>
-</head>
-<body>
-  <canvas id="barChart" width="400" height="300"></canvas>  
-  <script>
-      function name(params:type) {
-        
-      }
-*/         
-  </script>
-  </body>
-</html>
-
-
- );
- 
-
-}
+  
