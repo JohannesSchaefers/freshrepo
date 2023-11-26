32824604720e7897e65835f52d85b6ce8b7dbb39
johannesschaefers <info@iconicity.de>
2023-11-26 17:45:07 +0100

    10th biggest commit

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,6 +2,8 @@ import { useSignal } from "@preact/signals";
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
+
+  /*
   const count = useSignal(3);
   return (
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
@@ -22,4 +24,7 @@ export default function Home() {
       </div>
     </div>
   );
+  */
+<h1>Hello!</h1>
+
 }
