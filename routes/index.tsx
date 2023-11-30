b63edc714c6ecd6b97184539a323cee6ce4cf1d8
JohannesSchaefers <info@iconicity.de>
2023-11-30 22:12:10 +0100

    teco

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,7 +5,8 @@ export default function Home() {
    
   const count = useSignal(3);
   return (
-/*
+
+    /*
 
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
@@ -27,7 +28,9 @@ export default function Home() {
   );
   */
 
-    <h1>Just doin the do ... ... ... \./\./\./.</h1>);
+    <h1>Just doin the do ... ... ... \./\./\./.</h1>
+    
+    );
   
     }
 
