9753da18c0ffaef5a19fa53fbfed3f7507beceb0
JohannesSchaefers <info@iconicity.de>
2023-11-30 22:14:13 +0100

    altes

--- routes/index.tsx
+++ routes/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const count = useSignal(3);
   return (
 
-    /*
+   
 
     <div class="px-4 py-8 mx-auto bg-[#86efac]">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
@@ -26,9 +26,9 @@ export default function Home() {
       </div>
     </div>
   );
-  */
+ 
 
-    <h1>Just doin the do ... ... ... -- \./\./\./.</h1>
+   // <h1>Just doin the do ... ... ... -- \./\./\./.</h1>
     
     );
   
