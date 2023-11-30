6e226fbafb9da77d34131267362a37745c877ed7
JohannesSchaefers <info@iconicity.de>
2023-11-30 22:30:31 +0100

    count on you

--- routes/index.tsx
+++ routes/index.tsx
@@ -21,7 +21,8 @@ export default function Home() {
         <canvas id="myCanvas" width="300" height="150" style="border:1px solid grey"></canvas>
         <p class="my-4">
         <svg width="100" height="100">
-  <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
+          
+  <circle cx={count} cy="50" r="40" stroke="green" stroke-width="4" fill="yellow" />
 </svg>
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
