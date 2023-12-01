9178d585039b59f346bbf1187370d372466dca10
JohannesSchaefers <info@iconicity.de>
2023-12-01 22:04:43 +0100

    df

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,8 +7,7 @@ import * as $1 from "./routes/_404.tsx";
 import * as $2 from "./routes/_app.tsx";
 import * as $3 from "./routes/api/joke.ts";
 import * as $4 from "./routes/greet/[name].tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $6 from "./routes/test.tsx";
+import * as $5 from "./routes/test.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -18,8 +17,7 @@ const manifest = {
     "./routes/_app.tsx": $2,
     "./routes/api/joke.ts": $3,
     "./routes/greet/[name].tsx": $4,
-    "./routes/index.tsx": $5,
-    "./routes/test.tsx": $6,
+    "./routes/test.tsx": $5,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
--- routes/test.tsx
+++ routes/test.tsx
@@ -1,9 +1,6 @@
 
-
-
-
 // Your component definition
-function test() {
+export default function test(): import("https://esm.sh/v128/preact@10.18.1/src/jsx").JSXInternal.Element {
     return <div>test</div>;
 }
 
