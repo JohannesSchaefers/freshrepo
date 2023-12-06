8f39aa60c6b14fe361913c58f76426593ef3fde6
JohannesSchaefers <info@iconicity.de>
2023-12-06 12:53:55 +0100

    fgf

--- routes/index.tsx
+++ routes/index.tsx
@@ -21,6 +21,10 @@ export default function Home() {
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
         </p>
+
+// Wir fügen in html Interaktivität ein, indem wir eine Funktion aus einer Island aufrufen
+// So könnten wir auch einen über Rest geliefert Wert einfügen.
+
         <Counter1 count={count} />
       </div>
     </div>
