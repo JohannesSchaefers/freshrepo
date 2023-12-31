3563017cde1a9284e72ef013aa08739e88978f7c
JohannesSchaefers <info@iconicity.de>
2023-12-31 12:16:32 +0100

    dfd

--- routes/index.tsx
+++ routes/index.tsx
@@ -21,27 +21,9 @@ export default function Home() {
           Try updating this message in the
           <code class="mx-2">./routes/index.tsx</code> file, and refresh.
         </p>
-       
-      </div>
-
-
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">Welcome to the 232th biggest Fresh in town!</h1>
-        <p class="my-4">
-          Try updating this message in the
-          <code class="mx-2">./routes/index.tsx</code> file, and refresh.
-        </p>
         <Counter1 count={count} />
       </div>
-
+    
 
       <div class="flex p-6 font-mono">
   <div class="flex-none w-48 mb-10 relative z-10 before:absolute before:top-1 before:left-1 before:w-full before:h-full before:bg-teal-400">
