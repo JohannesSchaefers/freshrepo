8daab8becd26a467623c3df24417cfd1b39a8640
JohannesSchaefers <info@iconicity.de>
2024-01-07 21:59:41 +0100

    55

--- islands/Counter1.tsx
+++ islands/Counter1.tsx
@@ -1,32 +1,5 @@
-/*
-// Preact provides the thinnest possible Virtual DOM abstraction on top of the DOM
-import type { Signal } from "@preact/signals";
-import { Button } from "../components/Button.tsx";
 
-interface CounterProps {
-  count: Signal<number>;
-}
-
-// meine erste island
-// Islands ermöglichen dem Client zu interagieren. Islands sind isolierte Preact-Komponenten, die auf dem Server
-// gerendert werden und auf dem Client mit Wasser versorgt werden. Damit unterscheiden sie sich von allen anderen
-// Komponenten in Fresh, die nur auf dem Server gerendert werden.
-
-// Islands werden angelegt, in dem man eine Datei im islands/ Verzeichnis eines Fresh-Projects anlegt. Der Dateiname muss
-// ein PascalCase oder kebab-case Name der Island sein.
-
-export default function Counter1( props: CounterProps) {
-  return (
-    <div class="flex gap-8 py-6">
-      <Button onClick={() => props.count.value -= 1}>minus minus 1</Button>
-      <p class="text-3xl">{props.count}</p>
-      <Button onClick={() => props.count.value += 1}>plus plus 1</Button>          
-    </div>
-    
-  );
-}
-
-*/
+// programmed by ChatGPT
 
 import { h, FunctionalComponent } from 'preact';
 import { useEffect } from 'preact/hooks';
