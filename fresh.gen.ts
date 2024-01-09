afe77b929cbb2ec415c3d8b31110ff656e2a7c6a
JohannesSchaefers <info@iconicity.de>
2024-01-09 23:10:47 +0100

    sd

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,7 +2,7 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $TuFiles from "./routes/TuFiles.js";
+
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
@@ -14,8 +14,7 @@ import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/TuFiles.js": $TuFiles,
-    "./routes/_404.tsx": $_404,
+       "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
     "./routes/api/joke.ts": $api_joke,
     "./routes/greet/[name].tsx": $greet_name_,
